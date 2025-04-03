a41aba35e11e0c02ed3b41d2624176e9c07b433f
luisRoberto-401 <luis.fl142404@gmail.com>
2025-04-02 23:41:10 -0700

    cambio de BD
    MySQL no quiso chambear más

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -17,11 +17,11 @@ import { MateriaModule } from './materia/materia.module';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
+        type: 'postgres', // Cambiar de 'mysql' a 'postgres'
         host: configService.get('DB_HOST', 'localhost'),
-        port: configService.get<number>('DB_PORT', 3306),
-        username: configService.get('DB_USERNAME', 'root'),
-        password: configService.get('DB_PASSWORD', 'admin12'),
+        port: configService.get<number>('DB_PORT', 5432), // Cambiar puerto predeterminado de 3306 a 5432
+        username: configService.get('DB_USERNAME', 'postgres'), // Valor predeterminado típico para PostgreSQL
+        password: configService.get('DB_PASSWORD', 'admin'),
         database: configService.get('DB_NAME', 'horarios_escolares'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: configService.get('NODE_ENV') !== 'production',
